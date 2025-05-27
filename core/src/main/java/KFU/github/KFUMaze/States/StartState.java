3b5ed675813942400aa53a9e6bbf7125cc066018
KFUMaze <rakhmatullinkarim@gmail.com>
2025-05-27 18:44:57 +0300

    commit

---
+++ core/src/main/java/KFU/github/KFUMaze/States/StartState.java
@@ -0,0 +1,78 @@
+package KFU.github.KFUMaze.States;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import KFU.github.KFUMaze.Main;
+import KFU.github.KFUMaze.Sprites.Button;
+
+public class StartState extends State {
+
+    private Texture background;
+    Preferences prefs=Gdx.app.getPreferences("Game");
+    private Button play;
+    private Music SoundBtn=Gdx.audio.newMusic(Gdx.files.internal("Audio/ButtonSound.wav"));
+    private boolean[][] Save= {
+        {false, false, false},
+        {false, false, false},
+        {false, false, false},
+        {false, false, false},
+        {false, false, false}};
+
+    public StartState(GameStateManager gsm) {
+        super(gsm);
+        camera.setToOrtho(false, Main.WIDTH, Main.HEIGHT);
+        background = new Texture("StartBackground.png");
+
+        if(!prefs.contains("00")){
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 3; j++) {
+                    prefs.putBoolean(""+i+j,false);
+                }
+            }
+            prefs.flush();
+        }
+        else{
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 3; j++) {
+                    Save[i][j]=prefs.getBoolean(""+i+j);
+                }
+            }
+        }
+
+        play = new Button(1080 / 2 - 205, 1920 / 2 - 65, 410, 130, new Texture("Buttons/playbtn.png"));
+    }
+
+    @Override
+    public void handleInpute() {
+        if (Gdx.input.justTouched()) {
+            SoundBtn.play();
+            gsm.set(new MenuState(gsm, Save));
+        }
+    }
+
+    @Override
+    public void update(float dt) {
+        handleInpute();
+        //System.out.println(Gdx.input.isTouched());
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.setProjectionMatrix(camera.combined);
+        sb.begin();
+        sb.draw(background, 0, 0, Main.WIDTH, Main.HEIGHT);
+        play.draw(sb);
+        sb.end();
+    }
+
+    @Override
+    public void dispose() {
+        background.dispose();
+        play.dispose();
+        SoundBtn.dispose();
+    }
+}
